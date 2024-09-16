841f71342228c7e423adccff219d3f22a781297c
heiddley <heiddley@gmail.com>
2024-09-16 14:52:56 +1200

    Add files via upload

---
+++ membership.php
@@ -0,0 +1,9 @@
+﻿<!DOCTYPE html>
+<html>
+<head></head>
+<body>
+	Thank You <?php echo $_POST["name"]; ?><br>
+	Your membership has been accepted <br>
+	Email <?php echo $_POST["email"]; ?>
+</body>
+</html>
